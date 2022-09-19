314b5f46d5630cb1b1067963a491d985853bb942
Ryan <rdejana@us.ibm.com>
2022-09-19 14:27:05 -0600

    Create client.go

---
+++ client/client.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	cert, err := tls.LoadX509KeyPair("cert.pem", "key.pem")
+	if err != nil {
+		log.Fatal(err)
+	}
+	caCert, err := ioutil.ReadFile("cert.pem")
+	if err != nil {
+		log.Fatal(err)
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	// Create a HTTPS client and supply the created CA pool
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				RootCAs:      caCertPool,
+				Certificates: []tls.Certificate{cert},
+			},
+		},
+	}
+
+	// Request /hello over port 8080 via the GET method
+	//r, err := http.Get("http://localhost/hello")
+
+	r, err := client.Get("https://localhost/hello")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Read the response body
+	defer r.Body.Close()
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Print the response body to stdout
+	fmt.Printf("%s\n", body)
+}
